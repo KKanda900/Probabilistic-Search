59c43e2f2ce8440b17bbcd72bd3930e752b3b873
Karun Kanda <kkanda900@gmail.com>
2021-03-24 23:46:27 -0400

    another one

--- Map_Search.py
+++ Map_Search.py
@@ -11,9 +11,8 @@ class Terrians:
     cell_location = ()
 
     def __init__(self, i, j):
-        self.cell_location[0] = i
-        self.cell_location[1] = j
-        self.terrian_type = random.choice(types)
+        self.cell_location = (i, j)
+        self.terrian_type = random.choice(self.types)
         self.probability = 0.25
 
 class Map_Search:
@@ -33,11 +32,16 @@ class Map_Search:
         # generate all the locations
         for i in range(0, self.dim):
             for j in range(0, self.dim):
-
                 # if the target location == i and j then occupy i, j with the target
                 if target_location[0] == i and target_location[1] == j:
-                    map_board[i][j] = Terrians(i, j)
-                    map_board[i][j].contains_target = True
+                    self.map_board[i][j] = Terrians(i, j)
+                    self.map_board[i][j].contains_target = True
                 else:
                     # otherwise occupy the rest of the map with the random terrians
-                    map_board[i][j] = Terrians(i, j)
+                    self.map_board[i][j] = Terrians(i, j)
+    
+    def start(self):
+        print(self.map_board)
+
+board = Map_Search(20)
+board.start()
