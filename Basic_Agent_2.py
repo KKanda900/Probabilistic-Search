8eca0dfd3f3b40e9193cff6b6045158598fab5d7
Karun Kanda <kkanda900@gmail.com>
2021-04-07 17:41:27 -0400

    merge and update

--- Basic_Agent_2.py
+++ Basic_Agent_2.py
@@ -1,14 +1,24 @@
 import numpy, sys, random, math
-from Terrians import Terrians
+from Terrain import Terrians
 from Target import Target
 
 class Basic_Agent_2:
-    dim = 0
-    map_board = numpy.zeros((dim, dim), dtype=object)
-    target_info = None
-    belief_state = numpy.zeros((dim, dim), dtype=object)
-    previous_cells = [] # stores all the information about the previous cells here in the form (x, y)
+    
+    # information to store about the generated map
+    dim = 0 # default size 0 to start with 
+    map_board = numpy.zeros((dim, dim), dtype=object) # in the start create the map_board with the default dimension of 0
+    
+    # target info will hold all the information related to the target
+    target_info = None # set to None as default because the map wasn't generated yet
 
+    # information to store for the basic agent to use
+    belief_state = numpy.zeros((dim, dim), dtype=object) # tracks the belief of target being in cell_i given the observations
+    confidence_state = numpy.zeros((dim, dim), dtype=object) # tracks the confidence of the target actually being in cell_i given the belief state and current observations
+    previous_cells = [] # stores all the information about the previous cells here in the form (x, y)
+    distance_traveled = 0 # stores the distanced traveled from the current cell you start with 
+    num_searches = 0 # stores the number of searches made by the agent
+
+    # initialize information related to the the agent and the map itself
     def __init__(self, dim):
         self.dim = dim
         self.map_board = numpy.zeros((self.dim, self.dim), dtype=object)
@@ -20,17 +30,17 @@ class Basic_Agent_2:
         # generate all the locations
         for i in range(0, self.dim):
             for j in range(0, self.dim):
-                # if the target location == i and j then occupy i, j with the target
-                self.map_board[i][j] = Terrians(i, j)
-                self.belief_state[i][j] = 1/(self.dim*self.dim)
+                # create every i,j instance with the Terrian or belief at t=0 respectively
+                self.map_board[i][j] = Terrians(i, j) # generated map default values
+                self.belief_state[i][j] = 1/(self.dim*self.dim) # generated belief state default values
 
         # generate a random spot for the target to be located
-        indicies = list(range(0, self.dim))
-        target_location = (random.choice(indicies), random.choice(indicies))
-        terrian_type = self.map_board[target_location[0]][target_location[1]].terrian_type
-        self.target_info = Target(target_location[0], target_location[1], terrian_type)
-
-    # update the belief state based on bayesian updating (for basic agent 1)
+        indicies = list(range(0, self.dim)) # create a list of numbers to choose from
+        target_location = (random.choice(indicies), random.choice(indicies)) # get a random target location
+        terrian_type = self.map_board[target_location[0]][target_location[1]].terrian_type # the terrain of the target will be whatever map_board[x][y] is
+        self.target_info = Target(target_location[0], target_location[1], terrian_type) # store the information in the target_info variable
+    
+    # update the belief state based on bayesian updating (for basic agent 2)
     def bayesian_update(self, x, y):
         '''
         For Bayes updating we want to update the belief state with P(Target in Cell i | Observations at t and Failure in Cell j)
@@ -41,50 +51,131 @@ class Basic_Agent_2:
         '''
         # update the probability of failure for the previous cell
         curr_prev = self.previous_cells.pop(0)
-        prob_failure = 1 - (self.belief_state[curr_prev[0]][curr_prev[1]])
         # obtain the probability of the previous cell failing and the target is in the current cell
-        fnr = 1 # always 1
+        fnr = self.map_board[x][y].false_neg  # FNR of current cell
         # obtain the probability of the target being in the location based on the observation
-        curr_belief = self.belief_state[x][y]
-        # update the probability of the current cell
-        self.belief_state[x][y] = (curr_belief*1)/(prob_failure)
+        curr_cell_belief = self.belief_state[x][y]
+        prob_failure = (fnr * curr_cell_belief) + (1 - curr_cell_belief)
 
         '''
-        Step 2: Update the remaining probabilities so everything is equal to 1
+        Step 2: Update the remaining probabilities and Step 3: Update the confidence states for the current cell
         '''
         # go to each cell and make sure the total probability equals 1
         for i in range(self.dim):
-            sum_equal_1 = False
             for j in range(self.dim):
-                # if the i == x and j == y that means we are at the coordinate we just updated and we can just skip that
-                if i == x and j == y:
-                    pass
+                # if the false negative rates are same we have same terrain cell as the one search failed on in last step
+                if fnr == self.map_board[i][j].false_neg:
+                    self.belief_state[i][j] = (fnr * self.belief_state[i][j]) / prob_failure
+                    
+                    # obtain the probability of the Target being in Cell_i given the Observations at time t
+                    curr_belief = self.belief_state[i][j]
+                    # obtain the probability of Success in Cell_i given the Obseravations at time t
+                    current_success = 1-self.map_board[i][j].false_neg
+                    # update the current confidence state
+                    self.confidence_state[i][j] = curr_belief*current_success
                 else:
                     # if the other case isn't true that means we are at every other cell
-                    if self.belief_state[x][y] == 0:
-                        self.belief_state[x][y]+=1
-                    elif self.belief_state[x][y] > 0: # otherwise keep lowering the probability until we get back to the sum being 1
-                        self.belief_state[x][y]-=1
-                        if sum(self.belief_state) == 1:
-                            sum_equal_1 = True
-                            break
-                        else:
-                            pass
-            
-            if sum_equal_1 == True:
-                break
-    
-    def bayesian_update_v1(self, x, y):
-        '''
-        For Bayes updating we want to update the belief state with P(target found in cell_i | Observations_t)
-        '''
+                    self.belief_state[i][j] = self.belief_state[i][j] / prob_failure
 
-        '''
-        Step 1: 
-        '''
-    
+                    # obtain the probability of the Target being in Cell_i given the Observations at time t
+                    curr_belief = self.belief_state[i][j]
+                    # obtain the probability of Success in Cell_i given the Obseravations at time t
+                    current_success = 1-self.map_board[i][j].false_neg
+                    # update the current confidence state
+                    self.confidence_state[i][j] = curr_belief*current_success
+
     # start basic agent 2 here
-    def basic_agent_2(self):
-        print("Work on this tomorrow")
+    def start_agent(self, x, y):
 
-    
+        # termination variable to track if the target is found or not
+        target_found = False
+
+        # variables to keep track of for final result
+        tracking_distance = 0
+        searches = 0
+
+        # iterate through every cell over and over until the target is found
+        while target_found == False:
+
+            # if the target is found immediately exit out of the loop and return the results
+            if (x, y) == self.target_info.target_location:
+                rand = random.randint(0, 1)
+                if rand <= self.map_board[x][y].false_neg:
+                    # lets add the cell to the previous failure list
+                    self.previous_cells.append((x, y))
+
+                    # update the belief state based on equation 5 from the pdf write-up
+                    self.bayesian_update(x, y)
+
+                    # now iterate through all the cells that when i != x and j != y to find the new highest probabilities
+                    max_coord = (x, y, self.belief_state[x][y])
+                    for i in range(len(self.belief_state)):
+                        for j in range(len(self.belief_state)):
+                            if i != x and j != y:
+                                # if this condition returns true than we found the new highest probability out of all the coordinates
+                                if max_coord[2] < self.confidence_state[i][j]:
+                                    max_coord[0] = i
+                                    max_coord[1] = j
+                                    max_coord[2] = self.confidence_state[i][j]
+
+                                # if the condition returns true there are certain factors we have to take care of
+                                if max_coord[2] == self.confidence_state[i][j]:
+                                    # returns the coordinates with the lowest manhattan distance
+                                    smallest_coord = self.find_manhattan_distance((x, y), (max_coord[0], max_coord[1]), (i, j))
+                                    # checks if the distance and probability are the same if the condition is met below
+                                    if smallest_coord == (max_coord[0], max_coord[1]) and smallest_coord == (i, j):
+                                        arr_choices = [0, 1] # store 0 and 1 to pick from randomly to use to distinguish if probability and distance are equal
+                                        # if the random choice is 0 then choose max_coord 
+                                        if random.choice(arr_choices) == 0:
+                                            x = max_coord[0]
+                                            y = max_coord[1]
+                                        else:
+                                            # otherwise choose i, j if the above condition isn't satisified
+                                            x = i
+                                            y = j
+                                    else:
+                                        # if the condition above isn't true that means we can use smallest_coord effectively
+                                        x = smallest_coord[0]
+                                        y = smallest_coord[1]
+
+                else:
+                    # once we reach here we should exit out the loop and return the results
+                    target_found = True
+            else:
+                # lets add the cell to the previous failure list
+                self.previous_cells.append((x, y))
+
+                # update the belief state based on equation 5 from the pdf write-up
+                self.bayesian_update(x, y)
+
+                # now iterate through all the cells that when i != x and j != y to find the new highest probabilities
+                max_coord = (x, y, self.confidence_state[x][y])
+                for i in range(len(self.confidence_state)):
+                     for j in range(len(self.confidence_state)):
+                        if i != x and j != y:
+                            # if this condition returns true than we found the new highest probability out of all the coordinates
+                            if max_coord[2] < self.confidence_state[i][j]:
+                                max_coord[0] = i
+                                max_coord[1] = j
+                                max_coord[2] = self.confidence_state[i][j]
+
+                            # if the condition returns true there are certain factors we have to take care of
+                            if max_coord[2] == self.confidence_state[i][j]:
+                                # returns the coordinates with the lowest manhattan distance
+                                smallest_coord = self.find_manhattan_distance((x, y), (max_coord[0], max_coord[1]), (i, j))
+                                # checks if the distance and probability are the same if the condition is met below
+                                if smallest_coord == (max_coord[0], max_coord[1]) and smallest_coord == (i, j):
+                                    # store 0 and 1 to pick from randomly to use to distinguish if probability and distance are equal
+                                    arr_choices = [0, 1]
+                                    # if the random choice is 0 then choose max_coord
+                                    if random.choice(arr_choices) == 0:
+                                        x = max_coord[0]
+                                        y = max_coord[1]
+                                    else:
+                                        # otherwise choose i, j if the above condition isn't satisified
+                                        x = i
+                                        y = j
+                                else:
+                                    # if the condition above isn't true that means we can use smallest_coord effectively
+                                    x = smallest_coord[0]
+                                    y = smallest_coord[1]
--- basic_agent1.py
+++ basic_agent1.py
@@ -1,20 +1,26 @@
-import numpy, sys, random, math
+import numpy
+import sys
+import random
+import math
 from Terrain import Terrians
 from Target import Target
+
 
 class Basic_Agent_1:
     dim = 0
     map_board = numpy.zeros((dim, dim), dtype=object)
     target_info = None
     belief_state = numpy.zeros((dim, dim), dtype=object)
-    previous_cells = [] # stores all the information about the previous cells here in the form (x, y)
-    target_found=False
+    # stores all the information about the previous cells here in the form (x, y)
+    previous_cells = []
+    target_found = False
+
     def __init__(self, dim):
         self.dim = dim
         self.map_board = numpy.zeros((self.dim, self.dim), dtype=object)
         self.belief_state = numpy.zeros((self.dim, self.dim), dtype=object)
         self.generate_board()
-    
+
     # generate the board for the agent that is going to traverse
     def generate_board(self):
         # generate all the locations
@@ -27,8 +33,10 @@ class Basic_Agent_1:
         # generate a random spot for the target to be located
         indicies = list(range(0, self.dim))
         target_location = (random.choice(indicies), random.choice(indicies))
-        terrian_type = self.map_board[target_location[0]][target_location[1]].terrian_type
-        self.target_info = Target(target_location[0], target_location[1], terrian_type)
+        terrian_type = self.map_board[target_location[0]
+            ][target_location[1]].terrian_type
+        self.target_info = Target(
+            target_location[0], target_location[1], terrian_type)
 
     # update the belief state based on bayesian updating
     def bayesian_update(self, x, y):
@@ -53,8 +61,8 @@ class Basic_Agent_1:
         Step 2: Update the remaining probabilities so everything is equal to 1
         '''
         # go to each cell and make sure the total probability equals 1
+        sum_equal_1 = False
         for i in range(self.dim):
-            sum_equal_1 = False
             for j in range(self.dim):
                 # if the i == x and j == y that means we are at the coordinate we just updated and we can just skip that
                 if i == x and j == y:
@@ -62,80 +70,83 @@ class Basic_Agent_1:
                 else:
                     # if the other case isn't true that means we are at every other cell
                     if self.belief_state[x][y] == 0:
-                        self.belief_state[x][y]+=.01
-                    elif self.belief_state[x][y] > 0: # otherwise keep lowering the probability until we get back to the sum being 1
-                        self.belief_state[x][y]-=.01
+                        self.belief_state[x][y] += (1/self.dim)
+                    # otherwise keep lowering the probability until we get back to the sum being 1
+                    elif self.belief_state[x][y] > 0:
+                        if self.belief_state[x][y] != 0:
+                            self.belief_state[x][y] -= (1/self.dim)
                         print(sum(self.belief_state).any())
                         if sum(self.belief_state).any() == 1:
                             sum_equal_1 = True
                             break
                         else:
                             pass
-            
+
             if sum_equal_1 == True:
                 break
 
-    def calculate_neighbors(self,x,y):
-        neighbors=[]
-        if x+1<50 and y<50:
-            neighbors.append((x+1,y))
-        if x<50 and y+1<50:
-            neighbors.append((x,y+1))
-        if x-1<50 and y<50:
-            neighbors.append((x-1,y))
-        if x<50 and y-1<50:
-            neighbors.append((x,y-1))
+    def calculate_neighbors(self, x, y):
+        neighbors = []
+        if x+1 < 50 and y < 50:
+            neighbors.append((x+1, y))
+        if x < 50 and y+1 < 50:
+            neighbors.append((x, y+1))
+        if x-1 < 50 and y < 50:
+            neighbors.append((x-1, y))
+        if x < 50 and y-1 < 50:
+            neighbors.append((x, y-1))
         return neighbors
 
-    def clear_ties(self,board,x,y):
-        min_distance=0.0
-        ties=[]
-        j=0
+    def clear_ties(self, board, x, y):
+        min_distance = 0.0
+        ties = []
+        j = 0
         for i in board:
-            x_cord=i[0]
-            y_cord=i[1]
+            x_cord = i[0]
+            y_cord = i[1]
             distance = abs(x_cord-x) + abs(y_cord-y)
-            if j==0:
-                min_distance=distance
-                ties.append((x_cord,y_cord))
-                j=1
-            elif distance<min_distance:
-                min_distance=distance
+            if j == 0:
+                min_distance = distance
+                ties.append((x_cord, y_cord))
+                j = 1
+            elif distance < min_distance:
+                min_distance = distance
                 ties.clear()
-                ties.append((x_cord,y_cord))
-            elif min_distance==distance:
-                ties.append((x_cord,y_cord))
+                ties.append((x_cord, y_cord))
+            elif min_distance == distance:
+                ties.append((x_cord, y_cord))
         return ties[0]
 
-    def calculate_location(self,board):
-        location=()
-        ties=[]
-        max_val=0.0
-        for i in range(0,50):
-            for j in range(0,50):
-                if board[i][j]>max_val:
-                    max_val=board[i][j]
-                    location=(i,j)
+    def calculate_location(self, board):
+        location = ()
+        ties = []
+        max_val = 0.0
+        for i in range(0, 50):
+            for j in range(0, 50):
+                if board[i][j] > max_val:
+                    max_val = board[i][j]
+                    location = (i, j)
                     ties.clear()
                     ties.append(location)
-                elif board[i][j]==max_val:
-                    ties.append((i,j))
+                elif board[i][j] == max_val:
+                    ties.append((i, j))
         return ties
 
-    def basic_agent(self,x,y):
-        x_cord=x
-        y_cord=y
-        moves_counted=0
-        distance=0
-        while self.target_found==False:
-             if (x_cord,y_cord)==self.target_info.location and self.belief_state[x_cord][y_cord]==1-self.map_board[x_cord][y_cord].terrian_type.false_neg:
-                moves_counted+=1
-                self.target_found=True
+    def basic_agent(self, x, y):
+        x_cord = x
+        y_cord = y
+        moves_counted = 0
+        distance = 0
+        while self.target_found == False:
+            if (x_cord, y_cord) == self.target_info.location:
+                moves_counted += 1
+                self.target_found = True
             else:
                 self.previous_cells.append((x_cord,y_cord))
                 moves_counted+=1
                 self.bayesian_update(x_cord,y_cord)
                 locations=self.calculate_location(self.belief_state)
+                print(locations)
                 if len(locations)>1:
                     location=self.clear_ties(locations,x_cord,y_cord)
                     locations.clear()
@@ -151,4 +162,4 @@ x=random.randint(0,50)
 y=random.randint(0,50)
 info=Basic_Agent_1(50)
 print(info.basic_agent(x,y))           
-#self.start()
+# self.start()
