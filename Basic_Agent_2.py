240ca4ab5d44aa8b0879a478c5f697df3912e9a7
Agam1410 <48292808+Agam1410@users.noreply.github.com>
2021-04-07 16:42:50 -0400

    Update Basic_Agent_2.py

--- Basic_Agent_2.py
+++ Basic_Agent_2.py
@@ -1,13 +1,14 @@
 import numpy, sys, random, math
-from Terrians import Terrians
+from Terrain import Terrians
 from Target import Target
+
 
 class Basic_Agent_2:
     dim = 0
     map_board = numpy.zeros((dim, dim), dtype=object)
     target_info = None
     belief_state = numpy.zeros((dim, dim), dtype=object)
-    previous_cells = [] # stores all the information about the previous cells here in the form (x, y)
+    previous_cells = []  # stores all the information about the previous cells here in the form (x, y)
 
     def __init__(self, dim):
         self.dim = dim
@@ -41,40 +42,29 @@ class Basic_Agent_2:
         '''
         # update the probability of failure for the previous cell
         curr_prev = self.previous_cells.pop(0)
-        prob_failure = 1 - (self.belief_state[curr_prev[0]][curr_prev[1]])
         # obtain the probability of the previous cell failing and the target is in the current cell
-        fnr = 1 # always 1
+        fnr = self.map_board[x][y].false_neg  # FNR of current cell
         # obtain the probability of the target being in the location based on the observation
-        curr_belief = self.belief_state[x][y]
+        curr_cell_belief = self.belief_state[x][y]
+        prob_failure = (fnr * curr_cell_belief) + (1 - curr_cell_belief)
         # update the probability of the current cell
-        self.belief_state[x][y] = (curr_belief*1)/(prob_failure)
+        # self.belief_state[x][y] = curr_cell_belief/prob_failure
 
         '''
-        Step 2: Update the remaining probabilities so everything is equal to 1
+        Step 2: Update the remaining probabilities
         '''
         # go to each cell and make sure the total probability equals 1
         for i in range(self.dim):
-            sum_equal_1 = False
             for j in range(self.dim):
-                # if the i == x and j == y that means we are at the coordinate we just updated and we can just skip that
-                if i == x and j == y:
-                    pass
+                # if the false negative rates are same we have same terrain cell as the one search failed on in last step
+                if fnr == self.map_board[i][j].false_neg:
+                    self.belief_state[i][j] = (fnr * self.belief_state[i][j]) / prob_failure
                 else:
                     # if the other case isn't true that means we are at every other cell
-                    if self.belief_state[x][y] == 0:
-                        self.belief_state[x][y]+=1
-                    elif self.belief_state[x][y] > 0: # otherwise keep lowering the probability until we get back to the sum being 1
-                        self.belief_state[x][y]-=1
-                        if sum(self.belief_state) == 1:
-                            sum_equal_1 = True
-                            break
-                        else:
-                            pass
-            
-            if sum_equal_1 == True:
-                break
+                    self.belief_state[i][j] = self.belief_state[i][j] / prob_failure
     
     def bayesian_update_v1(self, x, y):
+
         '''
         For Bayes updating we want to update the belief state with P(target found in cell_i | Observations_t)
         '''
