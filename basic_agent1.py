5ab46d2824f40ce34844e4a9d2f5eec7abd55cce
Agam1410 <48292808+Agam1410@users.noreply.github.com>
2021-04-08 15:38:09 -0400

    it's done

--- basic_agent1.py
+++ basic_agent1.py
@@ -30,13 +30,16 @@ class Basic_Agent_1:
                 self.map_board[i][j] = Terrians(i, j)
                 self.belief_state[i][j] = 1/(self.dim*self.dim)
 
+        print(self.belief_state.sum())
+
         # generate a random spot for the target to be located
         indicies = list(range(0, self.dim))
         target_location = (random.choice(indicies), random.choice(indicies))
-        terrian_type = self.map_board[target_location[0]
-            ][target_location[1]].terrian_type
+        terrain_type = self.map_board[target_location[0]
+            ][target_location[1]].terrain_type
         self.target_info = Target(
-            target_location[0], target_location[1], terrian_type)
+            target_location[0], target_location[1], terrain_type)
+
 
     # update the belief state based on bayesian updating
     def bayesian_update(self, x, y):
@@ -48,39 +51,38 @@ class Basic_Agent_1:
         '''
         Step 1: Update the current cell then update the rest of the cells according to the cell
         '''
-        # update the probability of failure for the previous cell
-        curr_prev = self.previous_cells.pop(0)
         # obtain the probability of the previous cell failing and the target is in the current cell
         fnr = self.map_board[x][y].false_neg  # FNR of current cell
         # obtain the probability of the target being in the location based on the observation
         curr_cell_belief = self.belief_state[x][y]
         prob_failure = (fnr * curr_cell_belief) + (1 - curr_cell_belief)
         # update the probability of the current cell
-        # self.belief_state[x][y] = curr_cell_belief/prob_failure
+        self.belief_state[x][y] = (fnr*curr_cell_belief)/prob_failure
+        print("point:", x, y)
+        print("belief[x][y]:", self.belief_state[x][y])
 
         '''
         Step 2: Update the remaining probabilities so everything is equal to 1
         '''
         # go to each cell and make sure the total probability equals 1
-        sum_equal_1 = False
+
         for i in range(self.dim):
             for j in range(self.dim):
-                # if the false negative rates are same we have same terrain cell as the one search failed on in last step
-                if fnr == self.map_board[i][j].false_neg:
-                    self.belief_state[i][j] = (fnr * self.belief_state[i][j]) / prob_failure
-                else:
-                    # if the other case isn't true that means we are at every other cell
-                    self.belief_state[i][j] = self.belief_state[i][j] / prob_failure
+                if (i, j) != (x, y):
+                    self.belief_state[i][j] /= prob_failure
+
+        print("sum", self.belief_state.sum())
+
 
     def calculate_neighbors(self, x, y):
         neighbors = []
-        if x+1 < 50 and y < 50:
+        if x+1 < self.dim and y < self.dim:
             neighbors.append((x+1, y))
-        if x < 50 and y+1 < 50:
+        if x < self.dim and y+1 < self.dim:
             neighbors.append((x, y+1))
-        if x-1 < 50 and y < 50:
+        if x-1 < self.dim and y < self.dim:
             neighbors.append((x-1, y))
-        if x < 50 and y-1 < 50:
+        if x < self.dim and y-1 < self.dim:
             neighbors.append((x, y-1))
         return neighbors
 
@@ -102,14 +104,17 @@ class Basic_Agent_1:
                 ties.append((x_cord, y_cord))
             elif min_distance == distance:
                 ties.append((x_cord, y_cord))
-        return ties[0]
+        if len(ties)==1:
+            return ties[0]
+        else:
+            return random.choice(ties)
 
     def calculate_location(self, board):
         location = ()
         ties = []
         max_val = 0.0
-        for i in range(0, 50):
-            for j in range(0, 50):
+        for i in range(0, self.dim):
+            for j in range(0, self.dim):
                 if board[i][j] > max_val:
                     max_val = board[i][j]
                     location = (i, j)
@@ -125,28 +130,46 @@ class Basic_Agent_1:
         moves_counted=0
         distance=0
         while self.target_found==False:
-             if (x_cord,y_cord)==self.target_info.location and self.belief_state[x_cord][y_cord]==1-self.map_board[x_cord][y_cord].false_neg:
-                moves_counted+=1
-                self.target_found=True
-             else:
-                self.previous_cells.append((x_cord,y_cord))
-                moves_counted+=1
-                self.bayesian_update(x_cord,y_cord)
-                locations=self.calculate_location(self.belief_state)
-                print(locations)
-                if len(locations)>1:
-                    location=self.clear_ties(locations,x_cord,y_cord)
+            if (x_cord,y_cord)==self.target_info.location:
+                fnr=self.map_board[x_cord][y_cord].false_neg
+                rand=random.random()
+                if rand>fnr:
+                    moves_counted+=1
+                    self.target_found=True
+
+                else:
+                    self.previous_cells.append((x_cord,y_cord))
+                    moves_counted+=1
+                    self.bayesian_update(x_cord,y_cord)
+                    locations=self.calculate_location(self.belief_state)
+                    #print(locations)
+                    if len(locations)>1:
+                        location=self.clear_ties(locations,x_cord,y_cord)
+                        locations.clear()
+                        locations.append(location)
+                    coords=locations[0]
+                    distance+=abs(x_cord-coords[0])+abs(y_cord-coords[1])
+                    x_cord=coords[0]
+                    y_cord=coords[1]
+            else:
+                self.previous_cells.append((x_cord, y_cord))
+                moves_counted += 1
+                self.bayesian_update(x_cord, y_cord)
+                locations = self.calculate_location(self.belief_state)
+                # print(locations)
+                if len(locations) > 1:
+                    location = self.clear_ties(locations, x_cord, y_cord)
                     locations.clear()
                     locations.append(location)
-                coords=locations[0]
-                distance+=abs(x_cord-coords[0])+abs(y_cord-coords[1])
-                x_cord=coords[0]
-                y_cord=coords[1]
+                coords = locations[0]
+                distance += abs(x_cord - coords[0]) + abs(y_cord - coords[1])
+                x_cord = coords[0]
+                y_cord = coords[1]
 
-        return moves_counted+distance        
+        return moves_counted+distance
 
-x=random.randint(0,50)
-y=random.randint(0,50)
+x=random.randint(0,49)
+y=random.randint(0,49)
 info=Basic_Agent_1(50)
-print(info.basic_agent(x,y))           
+print(info.basic_agent(x,y))
 # self.start()
