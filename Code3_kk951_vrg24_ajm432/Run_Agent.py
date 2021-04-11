2bdd5cf43e98639fcaec840403edbb15f9e89402
Karun Kanda <kkanda900@gmail.com>
2021-04-10 23:41:53 -0400

    directory error

---
+++ Code3_kk951_vrg24_ajm432/Run_Agent.py
@@ -0,0 +1,25 @@
+import sys, basic_agent_1, basic_agent_2, advanced_agent
+
+'''
+Agent Driver
+
+Description: Runs the agents depending on command line arguments inputted.
+'''
+
+# initates the agent based on the requested command line argument
+if __name__ == "__main__":
+    # check if the user entered the right amount of arguments otherwise return failure
+    if len(sys.argv) != 2:
+        print("Usage Error:")
+        print("Enter One of the following:")
+        print("python Run_Agent.py ba1")
+        print("python Run_Agent.py ba2")
+        print("python Run_Agent.py adv")
+        sys.exit(1)
+
+    if sys.argv[1] == 'ba1':
+        basic_agent_1.start_agent()
+    elif sys.argv[1] == 'ba2':
+        basic_agent_2.start_agent()
+    elif sys.argv[1] == 'adv':
+        advanced_agent.start_agent()
---
+++ Code3_kk951_vrg24_ajm432/Terrain.py
@@ -0,0 +1,22 @@
+import random
+
+class Terrians:
+    types = ['flat', 'hilly', 'forested', 'caves']
+    terrain_type = None
+    # the probability of the target not being a cell given that we know it's in that particular cell
+    false_neg = 0.0 # by default its set to 0.0 but we can set it based on the terrian type
+    # the probability of the target being in a cell given that we know its not in a particular cell
+    false_pos = 0.0  # by default its 0.0 but we can increase this later
+    cell_location = () # store the location of the cell
+
+    def __init__(self, i, j):
+        self.cell_location = (i, j)
+        self.terrain_type = random.choice(self.types)
+        if self.terrain_type == 'flat':
+            self.false_neg = 0.10
+        elif self.terrain_type == 'hilly':
+            self.false_neg = 0.30
+        elif self.terrain_type == 'forested':
+            self.false_neg = 0.70
+        else:
+            self.false_neg = 0.90
