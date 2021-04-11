fba51dfb2dc14c3676e08388330a29f1cea8125d
Karun Kanda <kkanda900@gmail.com>
2021-04-10 23:51:15 -0400

    update

--- Code2_kk951_vrg24_ajm432/Target.py
+++
@@ -1,7 +0,0 @@
-class Target:
-    location = ()
-    terrain_location = None
-
-    def __init__(self, i, j, terrain_location):
-        self.location = (i, j)
-        self.terrain_location = terrain_location
